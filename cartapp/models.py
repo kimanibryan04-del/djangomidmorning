21516b9a9280b2033074e67f6a78ecb167a31fde
kimanibryan04-del <kimanibryan04@gmail.com>
2026-02-23 13:30:57 +0300

    initial commit

--- cartapp/models.py
+++ cartapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=100)
+    quantity= models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField()
+
+    def __str__(self):
+        return f"{self.product_name} - Ksh {self.price}"
+
